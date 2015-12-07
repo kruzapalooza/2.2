310cca4831ef2e2e60e336132ee03fee93872aaa
Marc Kruza <mkruza@gmail.com>
2015-12-06 19:45:26 -0500

    downloaded carousel files

--- source/assets/javascripts/carousel.js
+++ source/assets/javascripts/carousel.js
@@ -1,12 +1,12 @@
 //= require "jquery"
 
     $(document).ready(function() {
-      /*
+      
       $("#owl-demo").owlCarousel({
           autoPlay: 3000, //Set AutoPlay to 3 seconds
           items : 4,
           itemsDesktop : [1199,3],
           itemsDesktopSmall : [979,3]
       }); 
-      */
+      
     });
